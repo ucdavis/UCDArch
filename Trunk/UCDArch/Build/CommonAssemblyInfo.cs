db6e1e1844a29c47abe1de4290df4f3156165a56
Scott Kirkland <srkirkland@gmail.com>
2010-12-01 16:32:10 +0000

    Updated the build file to include a check to not run the regex replacement if the build number is not present.
    git-svn-id: https://ucdarch.svn.codeplex.com/svn@53345 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

--- Trunk/UCDArch/Build/CommonAssemblyInfo.cs
+++ Trunk/UCDArch/Build/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("5.4.3.2")]
+[assembly: AssemblyVersion("2.0.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
