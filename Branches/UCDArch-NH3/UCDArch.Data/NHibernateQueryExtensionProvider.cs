92d058fa2585b54b9bdeee29d9b0f1859e818c60
Scott Kirkland <srkirkland@gmail.com>
2010-12-07 16:41:08 +0000

    Added query extension methods for fetching and caching, including an implementation of a new IQueryExtensionProvider interface to handle the DI.
    git-svn-id: https://ucdarch.svn.codeplex.com/svn@53587 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ Branches/UCDArch-NH3/UCDArch.Data/NHibernateQueryExtensionProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using UCDArch.Core.PersistanceSupport;
+using NHibernate.Linq;
+
+namespace UCDArch.Data
+{
+    public class NHibernateQueryExtensionProvider : IQueryExtensionProvider 
+    {
+        public IQueryable<T> Cache<T>(IQueryable<T> queryable, string region)
+        {
+            var query = queryable.Cacheable();
+
+            if (region != null) query = query.CacheRegion(region);
+
+            return query;
+        }
+
+        public IQueryable<TOriginal> Fetch<TOriginal, TRelated>(IQueryable<TOriginal> queryable, Expression<Func<TOriginal, TRelated>> relationshipProperty, params Expression<Func<TRelated, TRelated>>[] thenFetchRelationship)
+        {
+            var ret = queryable.Fetch(relationshipProperty);
+
+            return thenFetchRelationship.Aggregate(ret, (current, fetchExpression) => current.ThenFetch(fetchExpression));
+        }
+
+        public IQueryable<TOriginal> FetchMany<TOriginal, TRelated>(IQueryable<TOriginal> queryable, Expression<Func<TOriginal, IEnumerable<TRelated>>> relationshipCollection, params Expression<Func<TRelated, IEnumerable<TRelated>>>[] thenFetchManyRelationship)
+        {
+            var ret = queryable.FetchMany(relationshipCollection);
+
+            return thenFetchManyRelationship.Aggregate(ret, (current, fetchExpression) => current.ThenFetchMany(fetchExpression));
+        }
+    }
+}
