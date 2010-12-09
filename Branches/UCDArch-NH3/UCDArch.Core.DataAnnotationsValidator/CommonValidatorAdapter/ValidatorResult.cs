f72eabbc83807a04d8f65461d1e9b48c5c2f877f
Scott Kirkland <srkirkland@gmail.com>
2010-12-09 18:03:13 +0000

    Created a new project for integrating DataAnnotations validation into UCDArch.  All tests pass, however DataAnnotations does not have a way to use [Required] when called on a list of objects (so that test is ignored).
    git-svn-id: https://ucdarch.svn.codeplex.com/svn@53653 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ Branches/UCDArch-NH3/UCDArch.Core.DataAnnotationsValidator/CommonValidatorAdapter/ValidatorResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UCDArch.Core.CommonValidator;
+
+namespace UCDArch.Core.DataAnnotationsValidator.CommonValidatorAdapter
+{
+    public class ValidationResult : IValidationResult
+    {
+        public Type ClassContext { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
