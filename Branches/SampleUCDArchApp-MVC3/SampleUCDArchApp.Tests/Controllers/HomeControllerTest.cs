18f4e7f2f6154ba10db06d97818518ca86a8893a
Scott Kirkland <srkirkland@gmail.com>
2011-01-20 00:53:16 +0000

    Branched from $/UCDArch/Trunk/SampleUCDArchApp
    git-svn-id: https://ucdarch.svn.codeplex.com/svn@55029 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ Branches/SampleUCDArchApp-MVC3/SampleUCDArchApp.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleUCDArchApp.Controllers;
+
+namespace SampleUCDArchApp.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            ViewDataDictionary viewData = result.ViewData;
+            Assert.AreEqual("Welcome to the UCDArch Sample Application!", viewData["Message"]);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
