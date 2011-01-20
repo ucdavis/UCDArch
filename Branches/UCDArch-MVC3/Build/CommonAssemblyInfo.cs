2199b2fa51f1969b6bc8707389d8942f5ede51be
Scott Kirkland <srkirkland@gmail.com>
2011-01-20 17:11:04 +0000

    Updated the build project (ILMerge task) to look in a new libs directory which will hold the necessary non-GAC references, like MVC3 and System.Web.WebPages (for tagbuilder).
    git-svn-id: https://ucdarch.svn.codeplex.com/svn@55050 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

--- Branches/UCDArch-MVC3/Build/CommonAssemblyInfo.cs
+++ Branches/UCDArch-MVC3/Build/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
