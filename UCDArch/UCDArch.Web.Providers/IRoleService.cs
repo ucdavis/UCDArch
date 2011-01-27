0ed58199904c78e85d694fe933317b42ee0c28a0
Scott Kirkland <srkirkland@gmail.com>
2011-01-27 14:16:34 -0800

    Added a new role provider which will use a WCF Role Service as a source.  The service uses a client class to do the actual fetching, and requires a serviceUrl, appName, and token to be correctly authenticated.

---
+++ UCDArch/UCDArch.Web.Providers/IRoleService.cs
@@ -0,0 +1,26 @@
+﻿namespace UCDArch.Web.Providers
+{
+    /// <summary>
+    /// Role Service for providing ASP.NET Role Provider data
+    /// </summary>
+    /// <remarks>
+    /// Role Provider Essential Methods:
+    /// IsUserInRole
+    /// GetAllRoles
+    /// GetRoleForUser
+    /// GetUsersInRole
+    /// RoleExists
+    /// </remarks>
+    public interface IRoleService
+    {
+        bool IsUserInRole(string application, string user, string role);
+
+        string[] GetAllRoles(string application);
+
+        string[] GetRolesForUser(string application, string user);
+
+        string[] GetUsersInRole(string application, string roleName);
+
+        bool RoleExists(string application, string role);
+    }
+}
