3fce710d66b3045d9db9e216e540ebae579c2dd9
Scott Kirkland <srkirkland@gmail.com>
2011-02-18 11:43:29 -0800

    Added customer map

---
+++ SampleUCDArchApp/SampleUCDArchApp.Core/Mappings/CustomerMap.cs
@@ -0,0 +1,20 @@
+﻿using FluentNHibernate.Mapping;
+using SampleUCDArchApp.Core.Domain;
+
+namespace SampleUCDArchApp.Core.Mappings
+{
+    public class CustomerMap : ClassMap<Customer>
+    {
+        public CustomerMap()
+        {
+            Id(x => x.Id, "CustomerID");
+
+            Map(x => x.CompanyName);
+            Map(x => x.ContactName);
+            Map(x => x.ContactName);
+            Map(x => x.Fax);
+
+            HasMany(x => x.Orders).Cascade.All();
+        }
+    }
+}
