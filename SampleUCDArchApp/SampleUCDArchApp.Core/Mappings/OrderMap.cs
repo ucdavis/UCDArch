55a193103261ed95290409e415b8068545136f82
Scott Kirkland <srkirkland@gmail.com>
2011-02-18 13:30:36 -0800

    Added fluent mappings for customer and order, removed product.  not currently hooked up to use fluent

---
+++ SampleUCDArchApp/SampleUCDArchApp.Core/Mappings/OrderMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using SampleUCDArchApp.Core.Domain;
+
+namespace SampleUCDArchApp.Core.Mappings
+{
+    public class OrderMap : ClassMap<Order>
+    {
+        public OrderMap()
+        {
+            Id(x => x.Id, "OrderID");
+
+            Map(x => x.OrderDate);
+            Map(x => x.ShipAddress);
+
+            References(x => x.OrderedBy);
+        }
+    }
+}
