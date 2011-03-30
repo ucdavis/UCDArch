9f9dd2b52fdfd7d0d1638d7a7e74dee7409ece7c
Scott Kirkland <srkirkland@gmail.com>
2011-03-30 11:50:42 -0700

    added in new jquery ui styles, and create a page for showing customers in a datatables infused table

--- SampleUCDArchApp/SampleUCDArchApp/Controllers/CustomerController.cs
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/CustomerController.cs
@@ -1,16 +1,25 @@
+using System.Linq;
 using System.Web.Mvc;
 using UCDArch.Web.Controller;
 using SampleUCDArchApp.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
 
 namespace SampleUCDArchApp.Controllers
 {
     public class CustomerController : ApplicationController
     {
+        private readonly IRepository<Customer> _customers;
+
+        public CustomerController(IRepository<Customer> customers)
+        {
+            _customers = customers;
+        }
+
         public ActionResult Index()
         {
-            var customers = Repository.OfType<Customer>().GetAll();
+            var customers = _customers.Queryable;
 
-            return View(customers);
+            return View(customers.ToList());
         }
     }
 }
