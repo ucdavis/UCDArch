ecb44c93e488b419ad93361a7d9485178091da2c
Scott Kirkland <srkirkland@gmail.com>
2011-06-20 02:46:01 -0700

    Added the service messages partial to sampleUcdArch, similar to the NuSurvey usage

--- SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs
@@ -4,6 +4,7 @@ using UCDArch.Web.Attributes;
 namespace SampleUCDArchApp.Controllers
 {
     [Version(MajorVersion = 3)]
+    [ServiceMessage("SampleUcdArchApp", ViewDataKey = "ServiceMessages", MessageServiceAppSettingsKey = "MessageService")]
     public abstract class ApplicationController : SuperController
     {
     }
