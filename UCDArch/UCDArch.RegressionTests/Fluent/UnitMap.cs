f9f501488317c89847d8288f74aea59e01b3b498
Scott Kirkland <srkirkland@gmail.com>
2012-05-15 10:18:16 -0700

    Added fluent nh repo tests for unit

---
+++ UCDArch/UCDArch.RegressionTests/Fluent/UnitMap.cs
@@ -0,0 +1,19 @@
+﻿using FluentNHibernate.Mapping;
+using UCDArch.RegressionTests.SampleMappings;
+
+namespace UCDArch.RegressionTests.Fluent
+{
+    public class UnitMap : ClassMap<Unit>
+    {
+        public UnitMap()
+        {
+            Table("Unit");
+            Id(x => x.Id).Column("UnitID");
+
+            Map(x => x.FISCode);
+            Map(x => x.ShortName);
+            Map(x => x.FullName);
+            Map(x => x.PPSCode);
+        }
+    }
+}
