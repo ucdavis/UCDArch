0666af0bb7df08491a849cbdf4a153135ff34f62
Scott Kirkland <srkirkland@gmail.com>
2012-12-21 15:01:04 -0800

    moved all samples to new sample controller
    will distribute sample controller along with boilerplate

--- SampleUCDArchApp/SampleUCDArchApp/Controllers/HomeController.cs
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/HomeController.cs
@@ -13,48 +13,7 @@ namespace SampleUCDArchApp.Controllers
 
             return View();
         }
-
-        public ViewResult Sample()
-        {
-            ViewBag.ErrorMessage = "Houston we have a problem... just kidding, this is only a test";
-            Message = "Sample Message from the Controller";
-            return View();
-        }
-
-        public ActionResult Full()
-        {
-            Message = "Here is a status message stored in TempData";
-            return View();
-        }
-
-        public ActionResult Form()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Form(string data)
-        {
-            Message = "The form was submitted successfully";
-
-            return View();
-        }
-
-        public ActionResult Table()
-        {
-            return View();
-        }
-
-        public ActionResult DataTable()
-        {
-            return View();
-        }
-
-        public ActionResult DisplayForm()
-        {
-            return View();
-        }
-
+        
         public ViewResult Placeholder()
         {
             return View();
