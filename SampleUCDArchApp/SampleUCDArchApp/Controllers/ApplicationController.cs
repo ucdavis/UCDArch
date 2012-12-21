81a02fb8c0b14586538b8bd930eebd9581bfc73c
Scott Kirkland <srkirkland@gmail.com>
2012-12-21 15:29:27 -0800

    the transform file will have service messages commented

--- SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs
@@ -3,9 +3,8 @@ using UCDArch.Web.Attributes;
 
 namespace SampleUCDArchApp.Controllers
 {
-    //TODO: Replace MessageServiceUrl with MessageServiceAppSettingsKey, and create an AppSettings key with the proper value
     [Version(MajorVersion = 3)]
-    [ServiceMessage("SampleUcdArchApp", ViewDataKey = "ServiceMessages", MessageServiceUrl = "https://test.caes.ucdavis.edu/Catbert4/public/message.svc")]
+    [ServiceMessage("SampleUcdArchApp", ViewDataKey = "ServiceMessages", MessageServiceAppSettingsKey = "MessageService")]
     public abstract class ApplicationController : SuperController
     {
     }
--- SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs.pp
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs.pp
@@ -3,9 +3,8 @@ using UCDArch.Web.Attributes;
 
 namespace $rootnamespace$.Controllers
 {
-    //TODO: Replace MessageServiceUrl with MessageServiceAppSettingsKey, and create an AppSettings key with the proper value
     [Version(MajorVersion = 3)]
-    [ServiceMessage("$rootnamespace$", ViewDataKey = "ServiceMessages", MessageServiceUrl = "https://test.caes.ucdavis.edu/Catbert4/public/message.svc")]
+    //[ServiceMessage("$rootnamespace$", ViewDataKey = "ServiceMessages", MessageServiceAppSettingsKey = "MessageService")]
     public abstract class ApplicationController : SuperController
     {
     }
