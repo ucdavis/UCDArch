13d02ba8e89b347a3a3e431b9e607a2ef177fc3d
Scott Kirkland <srkirkland@gmail.com>
2012-12-21 14:12:38 -0800

    forgot to remove bundles from ucdarch.mvc

--- SampleUCDArchApp/SampleUCDArchApp/App_Start/UCDArchBootstrapper.cs.pp
+++ SampleUCDArchApp/SampleUCDArchApp/App_Start/UCDArchBootstrapper.cs.pp
@@ -10,60 +10,29 @@ using UCDArch.Web.ModelBinder;
 [assembly: WebActivator.PreApplicationStartMethod(typeof(UCDArchBootstrapper), "PreStart")]
 namespace $rootnamespace$.App_Start
 {
-    public class UCDArchBootstrapper
+    /// <summary>
+    /// PreStart for the UCDArch Application configures the model binding, db, and IoC container
+    /// </summary>
+    public static void PreStart()
     {
-        /// <summary>
-        /// PreStart for the UCDArch Application configures the model binding, db, and IoC container
-        /// </summary>
-        public static void PreStart()
-        {
-            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+        ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
 
-            NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Customer).Assembly);
-
-            ConfigureAdditonalBundles();
-
-            IWindsorContainer container = InitializeServiceLocator();
-        }
-
-        private static void ConfigureAdditonalBundles()
-        {
-            const string dataTablesVersion = "1.9.4";
-
-            var bundles = BundleTable.Bundles;
+        NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Customer).Assembly);
             
-            bundles.Add(new ScriptBundle("~/bundles/datatables")
-                            .Include(string.Format("~/Scripts/DataTables-{0}/media/js/jquery.dataTables.js", dataTablesVersion))
-                            .Include("~/Scripts/datatables-bootstrap.js"));
+        IWindsorContainer container = InitializeServiceLocator();
+    }
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-                .Include("~/Scripts/bootstrap.js"));
+    private static IWindsorContainer InitializeServiceLocator()
+    {
+        IWindsorContainer container = new WindsorContainer();
 
-            // Note: Including bootstrap responsive-- comment it out if you don't need responsive css
-            bundles.Add(new StyleBundle("~/Content/css")
-                            .Include("~/Content/bootstrap.css")
-                            .Include("~/Content/bootstrap-custom.css")
-                            .Include("~/Content/bootstrap-responsive.css")
-                            .Include("~/Content/custom.css"));
+        ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
 
-            // Note: Including datatables helper css for bootstrap (http://datatables.net/blog/Twitter_Bootstrap_2)
-            bundles.Add(new StyleBundle(string.Format("~/Content/DataTables-{0}/media/css/dataTables", dataTablesVersion))
-                            .Include(string.Format("~/Content/DataTables-{0}/media/css/jquery.dataTables.css", dataTablesVersion))
-                            .Include(string.Format("~/Content/DataTables-{0}/media/css/datatables-bootstrap.css", dataTablesVersion)));
-        }
+        container.RegisterControllers(typeof(HomeController).Assembly);
+        ComponentRegistrar.AddComponentsTo(container);
 
-        private static IWindsorContainer InitializeServiceLocator()
-        {
-            IWindsorContainer container = new WindsorContainer();
+        ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
 
-            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
-
-            container.RegisterControllers(typeof(HomeController).Assembly);
-            ComponentRegistrar.AddComponentsTo(container);
-
-            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
-
-            return container;
-        }
+        return container;
     }
 }
