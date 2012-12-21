2a5a109af3246d06eb5d7344455e16d81e94ec15
Scott Kirkland <srkirkland@gmail.com>
2012-12-21 15:10:38 -0800

    include sample and application controllers

---
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs.pp
@@ -0,0 +1,11 @@
+﻿using UCDArch.Web.Controller;
+using UCDArch.Web.Attributes;
+
+namespace $rootnamespace$.Controllers
+{
+    [Version(MajorVersion = 3)]
+    [ServiceMessage("$rootnamespace$", ViewDataKey = "ServiceMessages", MessageServiceAppSettingsKey = "MessageService")]
+    public abstract class ApplicationController : SuperController
+    {
+    }
+}
---
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/SampleController.cs.pp
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCDArch.Web.Attributes;
+
+namespace $rootnamespace$.Controllers
+{
+    [HandleError]
+    [HandleTransactionsManually]
+    public class SampleController : ApplicationController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.ErrorMessage = "Houston we have a problem... just kidding, this is only a test";
+            Message = "Sample Message from the Controller";
+            
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(string data)
+        {
+            Message = "The form was submitted successfully";
+
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return View();
+        }
+
+        public ActionResult DataTable()
+        {
+            return View();
+        }
+
+        public ActionResult DisplayForm()
+        {
+            return View();
+        }
+    }
+}
