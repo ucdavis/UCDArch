4277a1982cd5ad64ff3a7ef99da54b60ea0d5dff
Scott Kirkland <srkirkland@gmail.com>
2012-12-20 16:35:24 -0800

    moved to bundleBootstrapper & custom /styles path

---
+++ SampleUCDArchApp/SampleUCDArchApp/App_Start/BundleBootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Optimization;
+using SampleUCDArchApp.App_Start;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(BundleBootstrapper), "PreStart")]
+namespace SampleUCDArchApp.App_Start
+{
+    /// <summary>
+    /// Configures additional bundles, like bootstrap and datatables
+    /// </summary>
+    public class BundleBootstrapper
+    {
+        private static void PreStart()
+        {
+            const string dataTablesVersion = "1.9.4";
+
+            var bundles = BundleTable.Bundles;
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables")
+                            .Include(string.Format("~/Scripts/DataTables-{0}/media/js/jquery.dataTables.js", dataTablesVersion))
+                            .Include("~/Scripts/datatables-bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js"));
+
+            // Note: Including bootstrap responsive-- comment it out if you don't need responsive css
+            bundles.Add(new StyleBundle("~/Content/styles")
+                            .Include("~/Content/bootstrap.css")
+                            .Include("~/Content/bootstrap-custom.css")
+                            .Include("~/Content/bootstrap-responsive.css")
+                            .Include("~/Content/custom.css"));
+
+            // Note: Including datatables helper css for bootstrap (http://datatables.net/blog/Twitter_Bootstrap_2)
+            bundles.Add(new StyleBundle(string.Format("~/Content/DataTables-{0}/media/css/dataTables", dataTablesVersion))
+                            .Include(string.Format("~/Content/DataTables-{0}/media/css/jquery.dataTables.css", dataTablesVersion))
+                            .Include(string.Format("~/Content/DataTables-{0}/media/css/datatables-bootstrap.css", dataTablesVersion)));
+        }
+    }
+}
--- SampleUCDArchApp/SampleUCDArchApp/App_Start/UCDArchBootstrapper.cs
+++ SampleUCDArchApp/SampleUCDArchApp/App_Start/UCDArchBootstrapper.cs
@@ -22,36 +22,8 @@ namespace SampleUCDArchApp.App_Start
             ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
 
             NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Customer).Assembly);
-
-            ConfigureAdditonalBundles();
-
-            IWindsorContainer container = InitializeServiceLocator();
-        }
-
-        private static void ConfigureAdditonalBundles()
-        {
-            const string dataTablesVersion = "1.9.4";
-
-            var bundles = BundleTable.Bundles;
             
-            bundles.Add(new ScriptBundle("~/bundles/datatables")
-                            .Include(string.Format("~/Scripts/DataTables-{0}/media/js/jquery.dataTables.js", dataTablesVersion))
-                            .Include("~/Scripts/datatables-bootstrap.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
-                .Include("~/Scripts/bootstrap.js"));
-
-            // Note: Including bootstrap responsive-- comment it out if you don't need responsive css
-            bundles.Add(new StyleBundle("~/Content/css")
-                            .Include("~/Content/bootstrap.css")
-                            .Include("~/Content/bootstrap-custom.css")
-                            .Include("~/Content/bootstrap-responsive.css")
-                            .Include("~/Content/custom.css"));
-
-            // Note: Including datatables helper css for bootstrap (http://datatables.net/blog/Twitter_Bootstrap_2)
-            bundles.Add(new StyleBundle(string.Format("~/Content/DataTables-{0}/media/css/dataTables", dataTablesVersion))
-                            .Include(string.Format("~/Content/DataTables-{0}/media/css/jquery.dataTables.css", dataTablesVersion))
-                            .Include(string.Format("~/Content/DataTables-{0}/media/css/datatables-bootstrap.css", dataTablesVersion)));
+            IWindsorContainer container = InitializeServiceLocator();
         }
 
         private static IWindsorContainer InitializeServiceLocator()
