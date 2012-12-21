bc5ee0bedecb2575800c7f03c9ea54347a371750
Scott Kirkland <srkirkland@gmail.com>
2012-12-21 15:23:55 -0800

    use url directly by default, with todo message

--- SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs.pp
+++ SampleUCDArchApp/SampleUCDArchApp/Controllers/ApplicationController.cs.pp
@@ -3,8 +3,9 @@ using UCDArch.Web.Attributes;
 
 namespace $rootnamespace$.Controllers
 {
+    //TODO: Replace MessageServiceUrl with MessageServiceAppSettingsKey, and create an AppSettings key with the proper value
     [Version(MajorVersion = 3)]
-    [ServiceMessage("$rootnamespace$", ViewDataKey = "ServiceMessages", MessageServiceAppSettingsKey = "MessageService")]
+    [ServiceMessage("$rootnamespace$", ViewDataKey = "ServiceMessages", MessageServiceUrl = "https://test.caes.ucdavis.edu/Catbert4/public/message.svc")]
     public abstract class ApplicationController : SuperController
     {
     }
