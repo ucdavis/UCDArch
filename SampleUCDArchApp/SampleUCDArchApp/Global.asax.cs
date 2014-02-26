744de1e40412cc0f68c37fa8761546d0ffc237b0
Scott Kirkland <srkirkland@gmail.com>
2014-02-26 11:32:17 -0800

    and update sub packages and reinstall for .net4.5.1

--- SampleUCDArchApp/SampleUCDArchApp/Global.asax.cs
+++ SampleUCDArchApp/SampleUCDArchApp/Global.asax.cs
@@ -19,7 +19,6 @@ namespace SampleUCDArchApp
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
